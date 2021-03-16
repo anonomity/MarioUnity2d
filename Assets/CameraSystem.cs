b1a87e158ad0f52aec4c6f3c1a305b9eb91ab468
anonomity <jlauravelez@gmail.com>
2021-03-16 09:49:55 +0100

    init commit

---
+++ Assets/CameraSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour
+{
+    private GameObject player;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+
+
+    }
+}
---
+++ Assets/Player_Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player_Health : MonoBehaviour
+{
+    public bool hasDied;
+    public int health;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.y < -6) {
+            Die();
+        }
+       
+    }
+
+    void Die () {
+        SceneManager.LoadScene("Prototype_1");
+    }
+}
