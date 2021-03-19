b8496f31036c556dfb039c17fbfb3b741fdb93e2
anonomity <jlauravelez@gmail.com>
2021-03-19 13:21:00 +0100

    March19

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -13,17 +13,21 @@ public class CameraSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if(GameObject.FindGameObjectWithTag("Player") != null){
+
         player = GameObject.FindGameObjectWithTag("Player");
+        }
 
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+       if(GameObject.FindGameObjectWithTag("Player") != null){ 
         float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
         float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
         gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
-
+       }
 
     }
 }
