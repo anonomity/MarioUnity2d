1ebfd1225cd7040685218b76260facfef81d8580
anonomity <jlauravelez@gmail.com>
2021-03-30 23:56:20 +0200

    character mechanics

--- Assets/Scripts/Player/Player_Move.cs
+++ Assets/Scripts/Player/Player_Move.cs
@@ -8,6 +8,7 @@ public class Player_Move : MonoBehaviour
 
     public int playerSpeed = 10;
     public int playerJumpPower = 1750;
+
     private float moveX;
     private float moveY;
     public bool isGrounded = true;
@@ -15,55 +16,45 @@ public class Player_Move : MonoBehaviour
 
     public bool isBig = false;
 
-
+    Rigidbody2D rb2d;
+    string buttonPressed;
 
     public static float distanceToBottomOfPlayer = 1f;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         PlayerRaycast();
-        // if (isGrounded == false)
-        // {
-        //     playerSpeed = 5;
-        // }
-        // else
-        // {
-        //     playerSpeed = 10;
-        // }
+
+
         AnimationChecks();
 
     }
 
     private void FixedUpdate()
     {
-        PlayerMove();
-    }
-    void PlayerMove()
-    {
         moveX = Input.GetAxis("Horizontal");
         moveY = Input.GetAxis("Vertical");
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-
-
             Jump();
-
-
-
-
         }
-
-
-
 
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
     }
 
 
+
     void Jump()
     {
-        SoundManager.PlaySound("jump");
-        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+        // SoundManager.PlaySound("jump");
+        float jumpVelocity = 30f;
+        rb2d.velocity = Vector2.up * jumpVelocity;
         isGrounded = false;
 
     }
@@ -111,3 +102,47 @@ public class Player_Move : MonoBehaviour
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// Interesting Jump Mechanics
+//   if (Input.GetButtonDown("Jump") && isGrounded)
+//         {
+//             Jump();
+//         }
+//         if (Input.GetKey(KeyCode.LeftArrow))
+//         {
+//             if (isGrounded)
+//             {
+//                 rb2d.velocity = new Vector2(-playerSpeed, rb2d.velocity.y);
+//             }
+//             else
+//             {
+//                 float midAirControl = 1f;
+//                 rb2d.velocity += new Vector2(-playerSpeed * midAirControl * Time.deltaTime, 0);
+//                 rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -playerSpeed, +playerSpeed), rb2d.velocity.y);
+//             }
+//         }
+//         else if (Input.GetKey(KeyCode.RightArrow))
+//         {
+//             rb2d.velocity = new Vector2(playerSpeed, rb2d.velocity.y);
+//         }
+//         else
+//         {
+//             if (isGrounded)
+//             {
+//                 rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+//             }
+//         }
